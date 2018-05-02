012262dc9d0b9f042e3536342023bc0ef3f548a6
hailv <www.htzs@qq.com>
2018-05-02 18:11:00 +0900

    'test'

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user/<int:id>')
+def index(id):
+    return '<h1>Hello,%s!</h1>'%id
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
