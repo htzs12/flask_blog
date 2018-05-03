9218ed6cb273e41af94cdbc90ff50cf900ec9d80
hailv <www.htzs@qq.com>
2018-05-03 11:18:43 +0900

    '第二次提交'

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
-from flask import Flask
+import config
+from datetime import datetime
+from flask import Flask,render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config.from_object(config)
+
+manager=Manager(app)
+bootstrap=Bootstrap(app)
+moment=Moment(app)
+
+class NameForm(Form):
+    name=StringField('what is your name?',validators=[Required()])
+    submit=SubmitField('Submit')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html',current_time=datetime.utcnow())
 
 @app.route('/user/<int:id>')
 def index(id):
     return '<h1>Hello,%s!</h1>'%id
 
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
