950aaf4c7045821f77622e756b9e0db8ffc63c6e
hailv <www.htzs@qq.com>
2018-05-03 16:57:49 +0900

    'migrate'

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
+import os
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+
 DEBUG=True
+
 SECRET_KEY='hard to guess string'
+
+SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(basedir,'data.sqlite')
+
+SQLALCHEMY_COMMIT_ON_TEARDOWN=True
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
