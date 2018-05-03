3acb3c12a25cd2c8f0b5d5d16d58c83206bfeb7a
hailv <www.htzs@qq.com>
2018-05-03 13:44:19 +0900

    'forms'

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import config
 from datetime import datetime
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 
@@ -15,20 +15,54 @@ manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name=StringField('what is your name?',validators=[Required()])
     submit=SubmitField('Submit')
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html',current_time=datetime.utcnow())
+'''
+@app.route('/',methods=['GET','POST'])
+
+def index():
+    name=None
+    form=NameForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data=''
+    return render_template('index.html',form=form,name=name)
+'''
+#重定向
+'''
+@app.route('/',methods=['GET','POST'])
+def index():
+    form=NameForm()
+    if form.validate_on_submit():
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
+'''
+#flash
+@app.route('/',methods=['GET','POST'])
+def index():
+    form=NameForm()
+    if form.validate_on_submit():
+        old_name=session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('looks like you have changed your name!')
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
+
+
+@app.route('/time/')
+def time():
+    return render_template('time.html',current_time=datetime.utcnow())
 
 @app.route('/user/<int:id>')
-def index(id):
+def user1(id):
     return '<h1>Hello,%s!</h1>'%id
 
 @app.route('/user/<name>')
-def user(name):
+def user2(name):
     return render_template('user.html',name=name)
 
 @app.errorhandler(404)
